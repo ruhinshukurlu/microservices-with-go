fd5c37e303428750f04ae17ff55225043b48f040
ruhinshukurlu <ruhinshukurlu@gmail.com>
2025-10-01 17:50:18 +0400

    external API communication (OSRM)

--- services/api-gateway/http.go
+++ services/api-gateway/http.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
 	"ride-sharing/shared/contracts"
 )
@@ -21,8 +23,26 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response := contracts.APIResponse{Data: "ok"}
+	jsonBody, _ := json.Marshal(reqBody)
+	reader := bytes.NewReader(jsonBody)
 
 	// TODO: Call Trip service
+	resp, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
+
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var respBody any
+
+	if err := json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
+		http.Error(w, "failed to parse JSON data from trip service", http.StatusBadRequest)
+		return
+	}
+
+	response := contracts.APIResponse{Data: respBody}
 	writeJSON(w, http.StatusCreated, response)
 }
--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,35 +1,29 @@
 package main
 
 import (
-	"context"
 	"log"
-	"ride-sharing/services/trip-service/internal/domain"
+	"net/http"
+	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
-	"time"
 )
 
 func main() {
-	ctx := context.Background()
-
 	inmemRepo := repository.NewInmemRepository()
-
 	service := service.NewService(inmemRepo)
+	mux := http.NewServeMux()
 
-	fare := &domain.RideFareModel{
-		UserID: "42",
+	httpHandler := h.HttpHandler{Service: service}
+
+	mux.HandleFunc("POST /preview", httpHandler.HandleTripPreview)
+
+	server := &http.Server{
+		Addr:    ":8083",
+		Handler: mux,
 	}
 
-	t, err := service.CreateTrip(ctx, fare)
-
-	if err != nil {
-		log.Println(err)
+	if err := server.ListenAndServe(); err != nil {
+		log.Printf("HTTP server error : %v ", err)
 	}
 
-	log.Println(t)
-
-	// keep the program running for now
-	for {
-		time.Sleep(time.Second)
-	}
 }
